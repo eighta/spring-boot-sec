67ecc8887b20664b86634680f1ffe2bdb49dcf22
eighta <ochoalarios@gmail.com>
2019-02-17 21:35:57 -0500

    base

---
+++ src/main/java/a8/Application.java
@@ -0,0 +1,13 @@
+package a8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("a8.controllers")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
